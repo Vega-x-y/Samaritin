5705f4472d9ce35e38c78995326d324e49d8e666
Martin Mahonga <martinmahonga@gmail.com>
2026-05-23 16:18:37 +0100

    feat: add NavLanding component and update welcome page

---
+++ resources/js/components/nav-landing.tsx
@@ -0,0 +1,141 @@
+import { Link, usePage } from '@inertiajs/react';
+import { Menu, Moon, Phone, Sun } from 'lucide-react';
+import { useAppearance } from '@/hooks/use-appearance';
+import { dashboard, login, home } from '@/routes';
+import { register } from '@/routes';
+import { Button } from './ui/button';
+import { Separator } from './ui/separator';
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+
+export default function NavLanding() {
+  const { auth, currentTeam } = usePage().props;
+  const { url } = usePage();
+  const dashboardUrl = currentTeam ? dashboard(currentTeam.slug) : '/';
+  const { appearance, updateAppearance } = useAppearance();
+
+  return (
+    <nav>
+      <div className="max-w-7xl m-auto flex items-center justify-between position-sticky top-0 z-100">
+        <Link href={home.get()} className='text-2xl font-bold text-primary'>Samaritain</Link>
+        <div className="md:flex hidden gap-6 items-center">
+          <Link href={home.get()} className={url === '/' ? 'text-primary' : 'text-sm hover:text-primary'}>Accueil</Link>
+          <Link href="/parcels" className={url.startsWith('/parcels') ? 'text-primary' : 'text-sm hover:text-primary'}>Parcelles à vendre</Link>
+          <Link href="/services" className={url.startsWith('/services') ? 'text-primary' : 'text-sm hover:text-primary'}>Services</Link>
+          <Button className='px-2.5 py-5 rounded-4xl'>
+            <a href="#" className='flex items-center gap-1'>
+              <Phone size={18} />
+              +242 06 800 71 38
+            </a>
+          </Button>
+          <Button 
+            variant="ghost"
+            onClick={() => updateAppearance(appearance === 'light' ? 'dark' : 'light')}
+            className='rounded-full cursor-pointer'>
+            {
+              appearance === 'light' ? <Sun size={18} /> : <Moon size={18} />
+            }
+          </Button>
+          <div className="flex items-center justify-end gap-4">
+            {auth.user ? (
+              <Link
+                href={dashboardUrl}
+                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+              >
+                Tableau de bord
+              </Link>
+              ) : (
+                <>
+                  <Button variant="ghost">
+                    <Link
+                      href={login()}
+                    >
+                      Se connecter
+                    </Link>
+                  </Button>
+                  <Button>
+                    <Link
+                      href={register()}
+                    >
+                      S'inscrire
+                    </Link>
+                  </Button>
+                </>
+              )}
+          </div>
+        </div>
+
+        {/* Mobile menu */}
+        <Sheet>
+          <SheetTrigger asChild className="md:hidden">
+            <Button variant="ghost">
+              <Menu size={24} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className='md:hidden'>
+            <SheetHeader>
+              <SheetTitle>
+                <Button 
+                  variant="ghost"
+                  onClick={() => updateAppearance(appearance === 'light' ? 'dark' : 'light')}
+                  className='rounded-full cursor-pointer text-start px-4'>
+                  {
+                    appearance === 'light' ? <Sun size={18} /> : <Moon size={18} />
+                  }
+                </Button>
+              </SheetTitle>
+            </SheetHeader>
+            <div className='flex flex-col gap-6 items-start px-4'>
+              <Link href={home.get()} className={url === '/' ? 'text-primary' : 'text-xl hover:text-primary'}>Accueil</Link>
+              <Link href="/parcels" className={url.startsWith('/parcels') ? 'text-primary' : 'text-xl hover:text-primary'}>Parcelles à vendre</Link>
+              <Link href="/services" className={url.startsWith('/services') ? 'text-primary' : 'text-xl hover:text-primary'}>Services</Link>
+            </div>
+            <SheetFooter>
+              <div className="flex flex-col w-full items-center justify-end gap-4">
+                <Button className='px-2.5 py-5 rounded-4xl w-full'>
+                  <a href="#" className='flex items-center gap-1'>
+                    <Phone size={18} />
+                    +242 06 800 71 38
+                  </a>
+                </Button>
+                <Separator/>
+                {auth.user ? (
+                  <Link
+                    href={dashboardUrl}
+                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                  >
+                    Tableau de bord
+                  </Link>
+                  ) : (
+                    <>
+                      <Button variant="outline" className='w-full'>
+                        <Link
+                          href={login()}
+                        >
+                          Se connecter
+                        </Link>
+                      </Button>
+                        <Button className='w-full'>
+                          <Link
+                            href={register()}
+                          >
+                            S'inscrire
+                          </Link>
+                        </Button>
+                      </>
+                    )}
+                </div>
+                </SheetFooter>
+              </SheetContent>
+          </Sheet>
+      </div>
+    </nav>
+  );
+}
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,41 +1,16 @@
-import { Head, Link, usePage } from '@inertiajs/react';
-import { dashboard, login } from '@/routes';
-import { register } from '@/routes';
+import { Head } from '@inertiajs/react';
+import NavLanding from '@/components/nav-landing';
 
 export default function Welcome() {
-    const { auth, currentTeam } = usePage().props;
-    const dashboardUrl = currentTeam ? dashboard(currentTeam.slug) : '/';
+    // const { auth, currentTeam } = usePage().props;
+    // const dashboardUrl = currentTeam ? dashboard(currentTeam.slug) : '/';
 
     return (
         <>
             <Head title="Welcome" />
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                    <nav className="flex items-center justify-end gap-4">
-                        {auth.user ? (
-                            <Link
-                                href={dashboardUrl}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                            >
-                                Dashboard
-                            </Link>
-                        ) : (
-                            <>
-                                <Link
-                                    href={login()}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                                >
-                                    Log in
-                                </Link>
-                                <Link
-                                    href={register()}
-                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                                >
-                                    Register
-                                </Link>
-                            </>
-                        )}
-                    </nav>
+                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-7xl">
+                    <NavLanding/>
                 </header>
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
